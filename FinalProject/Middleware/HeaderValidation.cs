7e08418e5b2a2817deacb95822766a9a9a48a327
Anthony Wong <AnthonyW@magenic.com>
2021-03-26 13:06:54 +0800

    Initial implementation for U27

---
+++ FinalProject/Middleware/HeaderValidation.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace FinalProject.Middleware
+{
+    /// <summary>
+    /// Static class to register this middleware into the application.
+    /// </summary>
+    public static class HeaderValidationExtension
+    {
+        public static IApplicationBuilder ApplyHeaderValidation(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<HeaderValidation>();
+            return app;
+        }
+    }
+
+    /// <summary>
+    /// Middleware class to implement header validation.
+    /// </summary>
+    public class HeaderValidation
+    {
+        private readonly RequestDelegate _next;
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="next"><see cref="RequestDelegate"/> automatically passed by the pipeline.</param>
+        public HeaderValidation(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Method that is used by the pipeline to execute this middleware.
+        /// </summary>
+        /// <param name="context">Context automatically injected by the request pipeline.</param>
+        /// <returns><see cref="Task"/></returns>
+        /// <remarks>Accept-Charset is forbidden to be modified, hence it is not included in the validation.</remarks>
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.Keys.Contains("x-api-version"))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("x-api-version header is missing.");
+                return;
+            }
+
+            if (!context.Request.Headers.Keys.Contains("x-api-key"))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("x-api-key header is missing.");
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ FinalProject/Swagger/AddRequiredHeaderParameter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace FinalProject.Swagger
+{
+    public class AddRequiredHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters ??= new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "x-api-key",
+                In = ParameterLocation.Header,
+                Description = "Ex. 1234567890",
+                Required = true,
+                Schema = new OpenApiSchema { Type = "string" }
+            });
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "x-api-version",
+                In = ParameterLocation.Header,
+                Description = "Ex. V1",
+                Required = true,
+                Schema = new OpenApiSchema { Type = "string" }
+            });
+        }
+    }
+}
