63d2a07f364fbc463be49e641877722affb40ef7
James Cusi <James.Cusi@ey.com>
2021-03-29 20:37:37 +0800

    Added OrdersController unit tests

---
+++ Tests/AccountControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FinalProject;
+using FinalProject.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Services;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class AccountControllerTest
+    { 
+    }
+}
